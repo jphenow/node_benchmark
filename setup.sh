81b1449261cce7abf8f03b01906213e24c2b107c
Jon Phenow <j.phenow@gmail.com>
2012-03-04 19:46:26 -0600

    added setup script

--- setup.sh
+++
@@ -1,6 +0,0 @@
-# Check that node exists
-# Check that npm exists
-# Check that db-msql installed via `npm install db-mysql`
-# Check that ruby exists
-# Check that php exists
-# Check that apache exists
