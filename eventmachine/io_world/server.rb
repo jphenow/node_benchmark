9a1ca3786733184a72be311392ddd6c5c224d92b
Jon Phenow <j.phenow@gmail.com>
2012-03-05 22:50:45 -0600

    grabs and joins DB for EM

--- eventmachine/io_world/server.rb
+++ eventmachine/io_world/server.rb
@@ -1,11 +1,23 @@
 #!/usr/bin/env ruby
 
+require 'sequel'
 require 'eventmachine'
 
 module Http
   def receive_data(data)
-    send_data "Hello World"
+    db :cb
     close_connection_after_writing
+  end
+
+  def db(callback)
+    db_defaults = {:host => "localhost", :user => "root", :password => nil,
+                   :port => 3306, :sock => nil, :adapter => 'mysql', :database => 'benchmark'}
+    dba = Sequel.connect db_defaults
+    self.send(callback, dba["SELECT name, email_address FROM user, profile WHERE profile.id = user.profile_id"].all)
+  end
+
+  def cb(data)
+    send_data data
   end
 end
 
