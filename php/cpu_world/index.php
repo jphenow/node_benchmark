0966daec601a1f297c3119c34607641aa7e2d715
Jon Phenow <j.phenow@gmail.com>
2012-03-10 13:23:19 -0600

    Forgot PHP likes every piece of punctuation on the keyboard..

--- php/cpu_world/index.php
+++ php/cpu_world/index.php
@@ -1,20 +1,19 @@
 <?php
 echo "Fibonacci: " . fibonacci(20);
 
-function fibonacci(y, cb){
-  fibminus2 = 0;
-  fibminus1 = 1;
-  fib = 0;
+function fibonacci($y){
+  $fibminus2 = 0;
+  $fibminus1 = 1;
+  $fib = 0;
 
-  if y == 0 or y == 1{
-    return y;
+  if($y == 0 || $y == 1){
+    return $y;
   }
-  for(i=2;i <= y; i++){
-    fib = fibminus1 + fibminus2;
-    fibminus2 = fibminus1;
-    fibminus1 = fib;
+  for($i=2;$i <= $y; $i++){
+    $fib = $fibminus1 + $fibminus2;
+    $fibminus2 = $fibminus1;
+    $fibminus1 = $fib;
   }
-
-  return fib;
+  return $fib;
 }
 ?>
