88896ed6c601fce62568d9aa223a82ef72746d53
Jon Phenow <j.phenow@gmail.com>
2012-03-10 14:17:07 -0600

    pumping up the DB size

--- db/db_filler.rb
+++ db/db_filler.rb
@@ -19,7 +19,7 @@ module SetupDb
 
     def generate_fillers
       fillers = []
-      1000.times do
+      10000.times do
         fillers << {:name => Faker::Name.name, :email_address => Faker::Internet.email}
       end
       fillers
